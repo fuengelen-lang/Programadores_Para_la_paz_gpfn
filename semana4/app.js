bc34dea9e2755509680220d287851da9072816bf
Gelen Fuentes <fuengelen@gmail.com>
2026-05-21 21:48:59 -0500

    Actividad semana 4

---
+++ semana4/app.js
@@ -0,0 +1,12 @@
+let numero1 = 10;
+let numero2 = 5;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("La suma es: " + suma);
+console.log("La resta es: " + resta);
+console.log("La multiplicación es: " + multiplicacion);
+console.log("La división es: " + division);
